27edf2f8c1c62dfbf30bfe59a1a0fdb8ee2c9811
RodrigoCS <rod.cervs@gmail.com>
2022-07-11 20:40:43 -0500

    DB Connection module Render homepage .env jwtsecret Co-authored-by: Triola Michelle <TMJSMC53@users.noreply.github.com>

--- db.js
+++ db.js
@@ -1,10 +1,30 @@
-const Mongoose = require("mongoose");
+const Mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
 
-const RemoteDB =
-  "mongodb+srv://JackTree_Coding:KL0eOVTMcWKI1Olu@cluster0.ydbf8.mongodb.net/?retryWrites=true&w=majority";
+// DECLARED DB VARIABLES
+let db,
+  dbConnectionStr = process.env.DB_STRING,
+  dbName = 'dogCentral';
+
 const connectDB = async () => {
-  Mongoose.connect(RemoteDB).then((client) => {
-    console.log("MongoDB Connected");
-  });
+  // CONNECT TO MONGO
+
+  // MongoClient.connect(dbConnectionStr).then((client) => {
+  //   console.log(`Connected to ${dbName} Database`);
+  //   db = client.db(dbName);
+  // });
+  const client = await MongoClient.connect(dbConnectionStr);
+  console.log(`Connected to ${dbName} Database`);
+  db = client.db(dbName);
+  return { db, dbConnectionStr, dbName };
 };
+
+// const RemoteDB =
+//   'mongodb+srv://JackTree_coding:7WZ8EkWlXMEo8jEg@cluster0.qyhpv.mongodb.net/?retryWrites=true&w=majority';
+// const connectDB = async () => {
+//   Mongoose.connect(RemoteDB).then((client) => {
+//     console.log('MongoDB Connected');
+//   });
+// };
 module.exports = connectDB;
