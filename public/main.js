55473961bc0c0fac121b9ff2d502d5bd4cc90622
Triola Jackson <triolajackson@gmail.com>
2022-09-07 14:01:56 -0500

    Refactored the html/css/js for the navbar

--- public/main.js
+++ public/main.js
@@ -1,19 +1,14 @@
-const overlay = document.getElementById("overlay");
-const sidebar = document.getElementById("sidebar");
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
 
-const menuOpen = document.querySelector(".menu__open");
-const menuClose = document.querySelector(".menu__close");
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
 
-// open/close hamburger menu
-menuOpen.addEventListener("click", () => {
-  sidebar.classList.remove("hidden");
-  sidebar.classList.add("show");
-  overlay.classList.remove("hidden");
-  menuOpen.classList.add("hidden");
-});
-
-menuClose.addEventListener("click", () => {
-  sidebar.classList.add("hidden");
-  overlay.classList.add("hidden");
-  menuOpen.classList.remove("hidden");
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
 });
