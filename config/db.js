a36561ba8fdd0c44f47468d1d18f9394fdcf4fbb
Triola Jackson <triolajackson@gmail.com>
2023-03-15 12:13:05 -0600

    made a change to the db.js file for cyclic

--- config/db.js
+++ config/db.js
@@ -5,6 +5,9 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.DB_STRING);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+    app.listen(process.env.PORT || PORT, () =>
+      console.log(`Server running on port ${PORT}, everything's good.`)
+    );
   } catch (err) {
     console.log(err);
     process.exit(1);
